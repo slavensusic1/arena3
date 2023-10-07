2a64637653315dfb8ee81b742eb4077473f72602
slavenSusic <118363308+slavenSusic@users.noreply.github.com>
2023-10-07 17:09:22 +0200

    undoen

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import Number from './Number'; 
+import React from 'react';
+import Number from './Number'; 
 
-// function CounterComponent() {
-//   return (
-//     <div className="py-10 px-5 md:py-20 md:px-36 bg-gray-800 text-white">
-//     <h2 class="text-5xl text-white font-bold text-left mb-4">What We Can Do for You</h2>
-//     <p class="text-left text-lg mb-6">
-//       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, fuga quam. Minima aperiam aliquam cum quisquam, laudantium corporis velit quaerat!
-//     </p>
-//     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-10">
-//       <Number n={100} title="Customers" />
-//       <Number n={50} title="Projects" />
-//       <Number n={10} title="Years of Experience" />
-//       <Number n={70} title="Solutions" />
-//     </div>
-//   </div>
-//   );
-// }
+function CounterComponent() {
+  return (
+    <div className="py-10 px-5 md:py-20 md:px-36 bg-gray-800 text-white">
+    <h2 class="text-5xl text-white font-bold text-left mb-4">What We Can Do for You</h2>
+    <p class="text-left text-lg mb-6">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, fuga quam. Minima aperiam aliquam cum quisquam, laudantium corporis velit quaerat!
+    </p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-10">
+      <Number n={100} title="Customers" />
+      <Number n={50} title="Projects" />
+      <Number n={10} title="Years of Experience" />
+      <Number n={70} title="Solutions" />
+    </div>
+  </div>
+  );
+}
 
-// export default CounterComponent;
+export default CounterComponent;
