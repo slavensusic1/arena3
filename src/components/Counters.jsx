2009dc3439eb9d67929b1ac234cf8ccdbc7d425b
slavenSusic <118363308+slavenSusic@users.noreply.github.com>
2023-10-10 11:24:11 +0200

    .

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -3,12 +3,12 @@ import Number from './Number';
 
 function CounterComponent() {
   return (
-    <div className="py-10 px-5 md:py-20 md:px-36 bg-gray-800 text-white">
-    <h2 class="text-3xl md:text-5xl text-white font-bold text-left mb-4 whitespace-pre-wrap">What We Can Do for You</h2>
-    <p class="text-left text-lg mb-6">
+    <div className="py-10 px-5 md:py-20 bg-gray-800 text-white">
+    <h2 class="text-3xl md:text-5xl text-white font-bold text-center mb-4">What We Can Do for You</h2>
+    <p class="text-center text-lg mb-6">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, fuga quam. Minima aperiam aliquam cum quisquam, laudantium corporis velit quaerat!
     </p>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-5">
       <Number n={100} title="Customers" />
       <Number n={50} title="Projects" />
       <Number n={10} title="Years of Experience" />
